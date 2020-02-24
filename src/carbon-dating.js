a0379af74c4df71ec31a3349ddbb93bb55f09a1a
Anna <a.sobolenko1194@gmail.com>
2020-02-24 14:51:38 +0300

    feat: task completed

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -1,7 +1,19 @@
 const MODERN_ACTIVITY= 15; 
 const HALF_LIFE_PERIOD= 5730;
 
-module.exports = function dateSample(/* sampleActivity */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function dateSample(sampleActivity) {
+  if(typeof sampleActivity === "undefined" || 
+     typeof sampleActivity !== "string" || 
+     sampleActivity <= 0 || 
+     !/^[\d\.]+$/.test(sampleActivity)|| 
+     parseFloat(sampleActivity) === NaN 
+     ){
+    return false
+  } 
+  let constantReaction = Math.LN2.toFixed(3) / HALF_LIFE_PERIOD;
+  let determineAge = Math.ceil(Math.log((MODERN_ACTIVITY / parseFloat(sampleActivity))) / constantReaction);
+  if (determineAge < 0) {
+    return false
+  }
+  return determineAge
 };
