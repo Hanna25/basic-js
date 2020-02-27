5de4181b8f7fd7af9d36f9fde781e1421618628e
Anna <a.sobolenko1194@gmail.com>
2020-02-28 00:30:22 +0300

    feat: task completed

--- src/what-season.js
+++ src/what-season.js
@@ -1,4 +1,16 @@
-module.exports = function getSeason(/* date */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function getSeason(date) {
+  if(typeof date == 'undefined'){
+    return 'Unable to determine the time of year!'
+  }else if(Object.prototype.toString.call(date) !== '[object Date]'){
+    throw Error()
+  }
+  if (0 === date.getMonth() || date.getMonth() === 1 || date.getMonth() === 11) {
+    return "winter"
+  } else if( 2 <= date.getMonth() && date.getMonth() < 5) {
+    return "spring"
+  } else if (5 <= date.getMonth() && date.getMonth() < 8) {
+    return "summer"
+  } else if (8 <= date.getMonth() && date.getMonth() < 11) {
+    return "autumn"
+  }
 };
